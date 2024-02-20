d8d254e87c230883f86f227a48a1b8b6f1f5b984
eristaufiqhidayat <eristaufiq@gmail.com>
2024-02-20 15:00:05 +0700

    1

--- praktek/php/mysqlConnect.php
+++ praktek/php/mysqlConnect.php
@@ -1,27 +1,15 @@
 <?php
-// Connecting, selecting database
-$link = mysql_connect('mysql_host', 'mysql_user', 'mysql_password')
-    or die('Could not connect: ' . mysql_error());
-echo 'Connected successfully';
-mysql_select_db('my_database') or die('Could not select database');
 
-// Performing SQL query
-$query = 'SELECT * FROM my_table';
-$result = mysql_query($query) or die('Query failed: ' . mysql_error());
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+$mysqli = mysqli_connect("localhost", "root", "", "latihan1");
 
-// Printing results in HTML
-echo "<table>\n";
-while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
-    echo "\t<tr>\n";
-    foreach ($line as $col_value) {
-        echo "\t\t<td>$col_value</td>\n";
-    }
-    echo "\t</tr>\n";
-}
-echo "</table>\n";
+$query = "SELECT * from datasiswa";
+$result = mysqli_query($mysqli, $query);
 
-// Free resultset
-mysql_free_result($result);
+/* numeric array */
+$row = mysqli_fetch_array($result, MYSQLI_NUM);
+printf("%s (%s)\n", $row[0], $row[1]);
 
-// Closing connection
-mysql_close($link);
+/* associative array */
+$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+printf("%s (%s)\n", $row["nama"], $row["alamat"]);
